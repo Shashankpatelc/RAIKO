16704ee065c6bc8147123c976647fb01f28e849d
Rabbie <118374636+Shashankpatelc@users.noreply.github.com>
2026-04-21 22:17:55 +0530

    Updated to use the previous convesation

--- modules/chat.py
+++ modules/chat.py
@@ -1,24 +1,48 @@
 from google import genai
-from config import API_KEY
 from modules import model_input
 from modules import model_output
 
-client = genai.Client(api_key=API_KEY)
-Model = "gemini-3-flash-preview"
+import config 
+import json
+
+# Model Credential
+client = genai.Client(api_key=config.API_KEY)
+Model = config.Model
+chat_session = client.chats.create(model=Model)  # Create a chat session 
 
 cond = True
 
+# Use to load the prevous chat to model
+def load_memory():
+    try:
+        with open('memory.json', 'r') as file:
+            summary = json.load(file)
+            chat_session.send_message(f"Here the summary of the previous conversation {summary['summary']}").text
+    except:
+        return None
+
+# Use to get the response from the model
 def get_respond(user_input):
-    response = client.models.generate_content(
-        model = Model,
-        contents = user_input 
-    )
+    response = chat_session.send_message(user_input)
     return response.text
 
+# Main where each function is called
 def main():
-    user_input = model_input.get()
+    user_input = model_input.get() # get the user input
     print()
+
+    # Exit the model
     if user_input.lower() == "bye":
+        summary = {
+             "summary" :  chat_session.send_message("Summarize the whole chat so you can remember that when i pass that.")
+            }
+        with open('memory.json', 'w') as file:
+            json.dump(summary, file)
         return False
-    model_output.put(get_respond(user_input))
+
+    # Get the response from model
+    replay = get_respond(user_input)
+
+    # Show the Output
+    model_output.put(replay)
     return True
