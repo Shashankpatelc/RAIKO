5ea5fe774477000ead7ef7c43a9853e80eac2bc3
Rabbie <118374636+Shashankpatelc@users.noreply.github.com>
2026-04-21 22:16:23 +0530

    RAIKO v0.2 - chat session management

--- raiko.py
+++ raiko.py
@@ -1,6 +1,12 @@
 import modules.chat as chat
+from google import genai
+import config 
+
+client = genai.Client(api_key=config.API_KEY)
 
 cond = True
+
+# Main loop for the coversation
 while cond:
     cond = chat.main()
     
