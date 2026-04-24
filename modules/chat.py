c4d4c7a371bb57662dd18204ce7efbc6d65b2880
Rabbie <118374636+Shashankpatelc@users.noreply.github.com>
2026-04-24 19:28:55 +0530

    Model Moved to LLM

--- modules/chat.py
+++ modules/chat.py
@@ -1,16 +1,15 @@
-from google import genai
 from modules import model_input
 from modules import model_output
 from modules import commands
 
+import ollama
 import config 
 import json
 
 # Model Credential
-client = genai.Client(api_key=config.API_KEY)
 Model = config.Model
-chat_session = client.chats.create(model=Model)  # Create a chat session 
 
+conversation = []
 cond = True
 
 # Use to load the prevous chat to model
@@ -18,14 +17,19 @@ def load_memory():
     try:
         with open('memory.json', 'r') as file:
             summary = json.load(file)
-            chat_session.send_message(f"Here the summary of the previous conversation {summary['summary']}").text
+            ollama.chat(model=Model, messages=summary)
     except:
         return None
 
 # Use to get the response from the model
 def get_respond(user_input):
-    response = chat_session.send_message(user_input)
-    return response.text
+    conversation.append({
+        'role' : 'user',
+        'content' : user_input
+        })
+    response: ollama.ChatResponse = ollama.chat(model=Model, messages=conversation)
+    conversation.clear()
+    return response.message.content
 
 # Main where each function is called
 def main():
@@ -35,7 +39,7 @@ def main():
     # Exit the model
     if user_input.lower() == "bye":
         summary = {
-             "summary" :  chat_session.send_message("Summarize the whole chat so you can remember that when i pass that in a new chat.").text
+             "summary" :  get_respond("Summarize the whole chat so you can remember that when i pass that in a new chat.")
             }
         with open('memory.json', 'w') as file:
             json.dump(summary, file)
