908807fee62a8e152cb2aa61f5dda9786313946a
Rabbie <118374636+Shashankpatelc@users.noreply.github.com>
2026-04-25 08:32:46 +0530

    Ollama with session management

--- modules/chat.py
+++ modules/chat.py
@@ -16,19 +16,34 @@ cond = True
 def load_memory():
     try:
         with open('memory.json', 'r') as file:
-            summary = json.load(file)
-            ollama.chat(model=Model, messages=summary)
+            data = json.load(file)
+            # print(data[0]['content']) # The Passing Data to the model
+            if data:
+                conversation.append({
+                    "role": "system",
+                    "content": f"Previous conversation summary: {data[0]['content']}"
+                })
+                response: ollama.ChatResponse = ollama.chat(model=Model, messages=conversation)
+                # print(response.message.content) # The response after the passing the summanry
     except:
+        print("Some Error while recovering the previous chat")
         return None
 
 # Use to get the response from the model
 def get_respond(user_input):
+    # Update the user conversation
     conversation.append({
-        'role' : 'user',
-        'content' : user_input
+            'role' : 'user',
+            'content' : user_input
         })
+    # Update the Model response
     response: ollama.ChatResponse = ollama.chat(model=Model, messages=conversation)
-    conversation.clear()
+    conversation.append(
+        {
+        'role' : 'model',
+        'content' : response.message.content
+        }
+    )
     return response.message.content
 
 # Main where each function is called
@@ -38,18 +53,21 @@ def main():
 
     # Exit the model
     if user_input.lower() == "bye":
-        summary = {
-             "summary" :  get_respond("Summarize the whole chat so you can remember that when i pass that in a new chat.")
-            }
+        # Get the summary of the chat and pass to the new list
+        summary = [{
+            "role": "system",
+            "content": get_respond("Summarize the whole chat so you can remember it.")
+        }]
+        # Write the summary to the memory.json file
         with open('memory.json', 'w') as file:
             json.dump(summary, file)
         return False
+    # Check the commands
     elif commands.check(user_input):
         print("Commmand found..............")
     else:
         # Get the response from model
         replay = get_respond(user_input)
-
         # Show the Output
         model_output.put(replay)
     return True
